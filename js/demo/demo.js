717abbd87df84f36babfcb18a291f2c8f4a14bea
hariza17 <locoariza17@gmail.com>
2016-03-17 19:05:34 -0500

    Iniciando el trabajo

---
+++ js/demo/demo.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+ * http://themerex.com/
+ * Copyright (c) 2015 ThemeREX;
+*/
+
+/*
+ * Demo theme functions.
+ * Required for Settings Pane and
+ * misc functions
+*/
+var Demo = function() {
+
+  // Demo AllCPForm Functions
+
+
+  // Demo Header Functions
+
+
+  return {
+   console.log("obj");
+  }
+}();
---
+++ js/pages/forms-wizard.js
@@ -0,0 +1,42 @@
+'use strict';
+//  Author: ThemeREX.com
+//  forms-wizard.html scripts
+//
+
+(function($) {
+
+    $(document).ready(function() {
+
+        "use strict";
+
+        // Init Theme Core
+        Core.init();
+
+        // Init Demo JS
+
+
+        // Form Wizard
+        var form = $("#form-wizard");
+
+        form.children(".wizard").steps({
+            headerTag: ".wizard-section-title",
+            bodyTag: ".wizard-section",
+            onStepChanging: function(event, currentIndex, newIndex) {
+                form.validate().settings.ignore = ":disabled,:hidden";
+                return form.valid();
+            },
+            onFinishing: function(event, currentIndex) {
+                form.validate().settings.ignore = ":disabled";
+                return form.valid();
+            },
+            onFinished: function(event, currentIndex) {
+                alert("Submitted!");
+            }
+        });
+
+
+
+
+    });
+
+})(jQuery);
