1f5943ad6801b84e09fa2a2b20ff5e3e7cc830d2
hariza17 <locoariza17@gmail.com>
2016-04-06 00:18:16 -0500

    Ultimo commit

--- js/mi_main.js
+++ js/mi_main.js
@@ -3,40 +3,43 @@
 //  index.html scripts
 //
 
-(function($) {
+(function ($) {
 
-    $(document).ready(function() {
-
-
-        var form = $("#form-wizard");
-
-        form.children(".wizard").steps({
-            headerTag: ".wizard-section-title",
-            bodyTag: ".wizard-section",
-            onStepChanging: function(event, currentIndex, newIndex) {
-                form.validate().settings.ignore = ":disabled,:hidden";
-                return form.valid();
-            },
-            onFinishing: function(event, currentIndex) {
-                form.validate().settings.ignore = ":disabled";
-                return form.valid();
-            },
-            onFinished: function(event, currentIndex) {
-                alert("Datos guardados con exito !");
-            }
-        });
-
-       $('#datetimepicker5').datetimepicker({
-            defaultDate: "10/01/2015",
-            pickDate: false
-        });
-
-       $('.datetimepicker').datetimepicker({
-            defaultDate: Date.now(),
-            pickDate: true
-        });
+   $(document).ready(function () {
 
 
-    });
+      var form = $("#form-wizard");
+
+      form.children(".wizard").steps({
+         headerTag: ".wizard-section-title",
+         stepsOrientation: "vertical",
+         bodyTag: ".wizard-section",
+         labels: {
+            next: "Siguiente",
+            previous: "Anterior",
+            finish: "Finish"
+         },
+         onStepChanging: function (event, currentIndex, newIndex) {
+            form.validate().settings.ignore = ":disabled,:hidden";
+            return form.valid();
+         },
+         onFinishing: function (event, currentIndex) {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+         },
+         onFinished: function (event, currentIndex) {
+            alert("Datos guardados con exito !");
+         }
+      });
+
+
+      $('.datetimepicker').datetimepicker({
+         defaultDate: Date.now(),
+         pickDate: true,
+         pickTime: false
+      });
+
+
+   });
 
 })(jQuery);
